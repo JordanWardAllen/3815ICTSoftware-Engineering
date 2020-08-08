d6bbcf35f109858db38520dcefe78365369fac87
JordanWardAllen <45648863+JordanWardAllen@users.noreply.github.com>
2020-08-08 12:59:00 +1000

    Cleaned up code for finalising prototyping

--- Milestone1/Pacman.py
+++ Milestone1/Pacman.py
@@ -16,6 +16,8 @@ from pygame.locals import (
 class Player(pygame.sprite.Sprite):
 
     def __init__(self, playerX, playerY):
+        # super().__init__()
+        pygame.sprite.Sprite.__init__(self)
         self.playerImg = pygame.image.load("player.png")
         self.playerImg = pygame.transform.scale(self.playerImg, (25, 25))
         self.playerX = playerX
@@ -28,7 +30,7 @@ class Player(pygame.sprite.Sprite):
 
 
     def playerMove(self):
-        vel = 1
+        vel = 3
         move_ticker = 0
         leftKey = True
         rightKey = True
@@ -43,7 +45,7 @@ class Player(pygame.sprite.Sprite):
             elif isColliding[0] == 'left':
                 leftKey = False
             elif isColliding[0] == 'right':
-                rightKey == False
+                rightKey = False
         else:
             leftKey = True
             rightKey = True
@@ -78,17 +80,13 @@ class Player(pygame.sprite.Sprite):
 
 class Wall(pygame.sprite.Sprite):
     def __init__(self, wallX,wallY, wallHeight, wallWidth):
+        super().__init__()
         self.wallColour = (255,255,255)
         self.wallX = wallX
         self.wallY = wallY
         self.wallHeight = wallHeight
         self.wallWidth = wallWidth
-        self.wall = pygame.draw.rect(screen, self.wallColour, (self.wallX, self.wallY, self.wallHeight, self.wallWidth))
-
-
-
-
-
+        self.rect = pygame.draw.rect(screen, self.wallColour, (self.wallX, self.wallY, self.wallHeight, self.wallWidth))
 
 
 
@@ -96,8 +94,7 @@ pygame.init()
 
 displayWindow = (600, 660)
 
-playerGroup = pygame.sprite.Group()
-blockedGroup = pygame.sprite.Group()
+
 
 # Set up the drawing window
 screen = pygame.display.set_mode(displayWindow)
@@ -106,7 +103,7 @@ pygame.display.set_caption("Pac Man")
 move_ticker = 0
 
 clock = pygame.time.Clock()
-vel = 2
+
 
 
 
@@ -115,14 +112,13 @@ def imageFunc():
     screen.blit(header, (0, 0))
 
 
-
 backgroundImg = pygame.image.load("colourmap.png")
 header = pygame.image.load("header.png")
-
-
 screen.blit(background,(0,0))
+player = Player(20, 150)
 
-player = Player(50, 50)
+playerGroup = pygame.sprite.Group()
+blockedGroup = pygame.sprite.Group()
 running = True
 isColliding = []
 
@@ -130,40 +126,68 @@ isColliding = []
 while running:
     keys = pygame.key.get_pressed()
     # Did the user click the window close button?
+    playerGroup.add(player)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-
-
     screen.fill((0,0,0))
+    wall1 = Wall(0, 80, 600, 10)
+    wall2 = Wall(0, 650, 600, 10)
+    wall3 = Wall(0, 0, 10, 660)
+    wall4 = Wall(590, 0, 10, 660)
+    wall5 = Wall(50, 130, 60, 40)
+    wall6 = Wall(490, 130, 60, 40)
+    wall7 = Wall(150, 130, 100, 40)
+    wall8 = Wall(350, 130, 100, 40)
+    wall9 = Wall(50, 210, 60, 40)
+    wall10 = Wall(490, 210, 60, 40)
+    wall11 = Wall(290, 90, 20, 80)
+    wall12 = Wall(290, 230, 20, 60)
+    wall13 = Wall(230, 210, 140, 20)
+    wall14 = Wall(10, 290, 100, 60)
+    wall15 = Wall(490, 290, 100, 60)
+    wall16 = Wall(490, 390, 100, 60)
+    wall17 = Wall(10, 390, 100, 60)
+    wall18 = Wall(50, 490, 60, 60)
+    wall19 = Wall(490, 490, 60, 60)
+    wall20 = Wall(350, 510, 100, 40)
+    wall21 = Wall(150, 510, 100, 40)
+    wall22 = Wall(290, 470, 20, 80)
+    wall23 = Wall(230, 450, 140, 20)
+    wall24 = Wall(50, 590, 140, 20)
+    wall25 = Wall(230, 590, 140, 20)
+    wall26 = Wall(410, 590, 140, 20)
+    wall27 = Wall(230, 325, 145, 85)
+    wall28 = Wall(150, 390, 40, 80)
+    wall29 = Wall(410, 390, 40, 80)
+    wall30 = Wall(410, 210, 40, 140)
+    wall31 = Wall(150, 210, 40, 140)
+    wall32 = Wall(190, 270, 60, 20)
+    wall33 = Wall(350, 270, 60, 20)
     imageFunc()
-    wall1 = Wall(0,0, 600, 20)
-    wall2 = Wall(0, 640, 600, 20)
-    wall3 = Wall(0, 0, 20, 660)
-    wall4 = Wall(580, 0, 20, 660)
 
 
     player.drawPlayer()
     player.playerMove()
-
     pressed_keys = pygame.key.get_pressed()
+
+
     for key_constant, pressed in enumerate(pressed_keys):
         if pressed:
-            if player.player.colliderect(wall1.wall) or player.player.colliderect(
-                    wall2.wall) or player.player.colliderect(wall3.wall) or player.player.colliderect(wall4.wall):
+            if player.player.colliderect(wall1) or player.player.colliderect(
+                    wall2) or player.player.colliderect(wall3) or player.player.colliderect(wall4) or player.player.colliderect(wall5):
+                print(isColliding)
+                print("collided")
                 if len(isColliding) == 0:
                     isColliding.append(pygame.key.name(key_constant))
-                    #key_name = pygame.key.name(key_constant)
+                    key_name = pygame.key.name(key_constant)
+                if len(isColliding) == 0:
+                    isColliding.append(pygame.key.name(key_constant))
+                    key_name = pygame.key.name(key_constant)
 
-                # print('worked')
-                # print(player.player.colliderect(wall1.wall))
-                # print(key_name)
             else:
                 isColliding.clear()
-
-
-
 
     # clock.tick(60)
     pygame.display.flip()
